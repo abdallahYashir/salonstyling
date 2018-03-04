4f1fb8284e7d2032131382d9ad173f60657597bc
AbdallahYashir <yashircr@gmail.com>
2018-03-04 18:07:53 +0400

    Added Days

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  resources :days
   devise_for :users
   resources :services
   resources :businesses
   resources :types
-  root 'types#index'
+  root 'businesses#index'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
