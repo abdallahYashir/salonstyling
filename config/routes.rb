3d2279d01aa0c481051ae599f17d5379557215a9
AbdallahYashir <yashircr@gmail.com>
2018-03-04 15:49:13 +0400

    Added services

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :services
   resources :businesses
   resources :types
   root 'types#index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180301124546) do
+ActiveRecord::Schema.define(version: 20180304114411) do
 
   create_table "businesses", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,16 @@ ActiveRecord::Schema.define(version: 20180301124546) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["type_id"], name: "index_businesses_on_type_id"
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.float "price"
+    t.integer "business_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["business_id"], name: "index_services_on_business_id"
   end
 
   create_table "types", force: :cascade do |t|
